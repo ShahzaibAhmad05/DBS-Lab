5ec15882613c05b74c542501d52c1bc9a46f8ccc
Shahzaib <shahzaibahmad6789@gmail.com>
2025-10-07 14:40:29 +0500

    add: mySQL queries for lab-1 tasks

---
+++ Lab-1.sql
@@ -0,0 +1,9 @@
+USE sakila;
+
+-- Task 1: Select the columns city_id and country_id from the city table.
+SELECT city_id, country_id
+FROM city;
+
+-- Task 2: Retrieve the first and last names of actors.
+SELECT first_name, last_name
+FROM actor;
